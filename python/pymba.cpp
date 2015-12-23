ae05c2e3bf6106ec3940e4a422da3165b98087e6
Denis Demidov <dennis.demidov@gmail.com>
2015-12-23 12:58:56 +0300

    Allow custom initial approximation

--- python/pymba.cpp
+++ python/pymba.cpp
@@ -2,11 +2,14 @@
 #include <string>
 #include <sstream>
 #include <memory>
+#include <functional>
 
 #include <mba/mba.hpp>
 
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include <pybind11/functional.h>
+#include <pybind11/stl.h>
 
 namespace py = pybind11;
 
@@ -17,15 +20,87 @@ void precondition(bool cond, std::string error_message) {
 template <unsigned NDim>
 struct python_mba {
     python_mba(
+            py::array_t<double> &lo,
+            py::array_t<double> &hi,
+            py::array_t<int>    &grid,
+            py::array_t<double> &coo,
+            py::array_t<double> &val,
+            const std::function<double(std::vector<double>)> &f,
+            int max_levels = 8,
+            double tol = 1e-8,
+            double min_fill = 0.5
+            )
+    {
+        boost::function<double(const boost::array<double,NDim>&)> initial =
+            [f](const boost::array<double, NDim> &a) {
+                return f(std::vector<double>(boost::begin(a), boost::end(a)));
+            };
+
+        init(lo, hi, grid, coo, val, initial, max_levels, tol, min_fill);
+    }
+
+    python_mba(
+            py::array_t<double> &lo,
+            py::array_t<double> &hi,
+            py::array_t<int>    &grid,
+            py::array_t<double> &coo,
+            py::array_t<double> &val,
+            int max_levels = 8,
+            double tol = 1e-8,
+            double min_fill = 0.5
+            )
+    {
+        boost::function<double(const boost::array<double,NDim>&)> none;
+        init(lo, hi, grid, coo, val, none, max_levels, tol, min_fill);
+    }
+
+    py::array_t<double> apply(py::array_t<double> &_coo) const {
+        typedef boost::array<double, NDim> point;
+
+        py::buffer_info coo = _coo.request();
+
+        const size_t ndim = coo.ndim;
+
+        precondition(ndim >= 2 && coo.shape[ndim-1] == NDim,
+                "coo should be a n x " + std::to_string(NDim) + " matrix"
+                );
+
+        std::vector<size_t> strides(ndim-1);
+        std::vector<size_t> shape(ndim-1);
+
+        shape.back() = coo.shape[ndim-2];
+        strides.back() = sizeof(double);
+        for(int i = ndim-2; i > 0; --i) {
+            shape[i-1] = coo.shape[i-1];
+            strides[i-1] = strides[i] * shape[i];
+        }
+
+        const size_t n = coo.count / NDim;
+
+        const point *coo_begin = static_cast<const point*>(coo.ptr);
+        const point *coo_end   = coo_begin + n;
+
+        std::vector<double> val(n);
+
+        std::transform(coo_begin, coo_end, val.begin(), std::ref(*m));
+
+        return py::array(py::buffer_info(val.data(), sizeof(double),
+                    py::format_descriptor<double>::value(),
+                    ndim-1, shape, strides));
+    }
+
+    std::shared_ptr< mba::MBA<NDim> > m;
+
+    void init(
             py::array_t<double> &_lo,
             py::array_t<double> &_hi,
             py::array_t<int>    &_grid,
             py::array_t<double> &_coo,
             py::array_t<double> &_val,
+            boost::function<double(const boost::array<double,NDim>&)> initial,
             int max_levels = 8,
             double tol = 1e-8,
-            double min_fill = 0.5,
-            bool use_linear = true
+            double min_fill = 0.5
             )
     {
         typedef boost::array<size_t, NDim> index;
@@ -67,49 +142,18 @@ struct python_mba {
         std::copy_n(static_cast<const double*>(hi.ptr), n, boost::begin(cmax));
         std::copy_n(static_cast<const int*>(grid.ptr), n, boost::begin(grid_size));
 
+        if (!initial) {
+            initial = mba::linear_approximation<NDim>(coo_begin, coo_end, val_begin);
+        }
+
         m = std::make_shared< mba::MBA<NDim> >(
                 cmin, cmax, grid_size,
                 coo_begin, coo_end, val_begin,
-                max_levels, tol, min_fill, use_linear
+                max_levels, tol, min_fill, initial
                 );
     }
 
-    py::array_t<double> apply(py::array_t<double> &_coo) const {
-        typedef boost::array<double, NDim> point;
 
-        py::buffer_info coo = _coo.request();
-
-        const size_t ndim = coo.ndim;
-
-        precondition(ndim >= 2 && coo.shape[ndim-1] == NDim,
-                "coo should be a n x " + std::to_string(NDim) + " matrix"
-                );
-
-        std::vector<size_t> strides(ndim-1);
-        std::vector<size_t> shape(ndim-1);
-
-        shape.back() = coo.shape[ndim-2];
-        strides.back() = sizeof(double);
-        for(int i = ndim-2; i > 0; --i) {
-            shape[i-1] = coo.shape[i-1];
-            strides[i-1] = strides[i] * shape[i];
-        }
-
-        const size_t n = coo.count / NDim;
-
-        const point *coo_begin = static_cast<const point*>(coo.ptr);
-        const point *coo_end   = coo_begin + n;
-
-        std::vector<double> val(n);
-
-        std::transform(coo_begin, coo_end, val.begin(), std::ref(*m));
-
-        return py::array(py::buffer_info(val.data(), sizeof(double),
-                    py::format_descriptor<double>::value(),
-                    ndim-1, shape, strides));
-    }
-
-    std::shared_ptr< mba::MBA<NDim> > m;
 };
 
 
@@ -125,7 +169,7 @@ void register_mba(py::module &m) {
                     py::array_t<int>&,
                     py::array_t<double>&,
                     py::array_t<double>&,
-                    int, double, double, bool
+                    int, double, double
                 >(), "Constructor",
                 py::arg("lo"),
                 py::arg("hi"),
@@ -134,8 +178,26 @@ void register_mba(py::module &m) {
                 py::arg("val"),
                 py::arg("max_levels") = 8,
                 py::arg("tol") = 1e-8,
-                py::arg("min_fill") = 0.5,
-                py::arg("use_linear") = true
+                py::arg("min_fill") = 0.5
+            )
+        .def(py::init<
+                    py::array_t<double>&,
+                    py::array_t<double>&,
+                    py::array_t<int>&,
+                    py::array_t<double>&,
+                    py::array_t<double>&,
+                    const std::function<double(std::vector<double>)>&,
+                    int, double, double
+                >(), "Constructor",
+                py::arg("lo"),
+                py::arg("hi"),
+                py::arg("grid"),
+                py::arg("coo"),
+                py::arg("val"),
+                py::arg("initial"),
+                py::arg("max_levels") = 8,
+                py::arg("tol") = 1e-8,
+                py::arg("min_fill") = 0.5
             )
         .def("__call__", &python_mba<NDim>::apply)
         .def("__repr__", [](const python_mba<NDim> &m){
